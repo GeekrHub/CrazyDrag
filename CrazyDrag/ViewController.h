61c8b72d23f6a23d033ae33c9ad0767e62074756
柳敏 <liumin441736571@gmail.com>
2016-05-21 21:23:44 +0800

    Initial Commit

---
+++ CrazyDrag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrazyDrag
+//
+//  Created by JERRY on 16/5/21.
+//  Copyright © 2016年 JERRY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CrazyDrag/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CrazyDrag
+//
+//  Created by JERRY on 16/5/21.
+//  Copyright © 2016年 JERRY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
